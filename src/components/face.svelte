7b16825f2fcff0d025df583a21b54656f6170df4
boladss <anton.boladss@gmail.com>
2025-01-14 16:17:55 +0800

    feat: add interaction hover effect

--- src/components/face.svelte
+++ src/components/face.svelte
@@ -1,5 +1,7 @@
 <script>
   let { pieces, large = false } = $props();
+  
+  let interactable = true;
 </script>
 
 <!-- Print face on its own -->
@@ -7,7 +9,11 @@
 <div class="size-fit p-4 rounded-2xl bg-gray-900">
   <div class="grid grid-cols-2 grid-rows-2 shadow-[0_0_10px_0px]">
     {#each pieces as piece}
-      <div class="size-16 shadow-md" style:background-color={piece.color}></div>
+      <div 
+        class="size-16 shadow-md {interactable ? 'hover:shadow-[0_0_5px_5px_red] hover:z-10' : ''}" 
+        style:background-color={piece.color}
+        >
+      </div>
     {/each}
   </div>
 </div>
