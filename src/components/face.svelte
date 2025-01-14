298783d0d235be7052af659f35aefa7f014bc598
boladss <anton.boladss@gmail.com>
2025-01-14 16:00:57 +0800

    refactor: change rendering to each

--- src/components/face.svelte
+++ src/components/face.svelte
@@ -6,10 +6,9 @@
 {#if large === true}
 <div class="size-fit p-4 rounded-2xl bg-gray-900">
   <div class="grid grid-cols-2 grid-rows-2 shadow-[0_0_10px_0px]">
-    <div class="size-16 shadow-md" style:background-color={pieces[0].color}></div>
-    <div class="size-16 shadow-md" style:background-color={pieces[1].color}></div>
-    <div class="size-16 shadow-md" style:background-color={pieces[2].color}></div>
-    <div class="size-16 shadow-md" style:background-color={pieces[3].color}></div>
+    {#each pieces as piece}
+      <div class="size-16 shadow-md" style:background-color={piece.color}></div>
+    {/each}
   </div>
 </div>
 
@@ -17,10 +16,9 @@
 {:else}
 <div class="size-fit p-2 rounded-xl bg-gray-900">
   <div class="grid grid-cols-2 grid-rows-2 shadow-[0_0_5px_0px]">
-    <div class="size-8 shadow-sm" style:background-color={pieces[0].color}></div>
-    <div class="size-8 shadow-sm" style:background-color={pieces[1].color}></div>
-    <div class="size-8 shadow-sm" style:background-color={pieces[2].color}></div>
-    <div class="size-8 shadow-sm" style:background-color={pieces[3].color}></div>
+    {#each pieces as piece}
+      <div class="size-8 shadow-sm" style:background-color={piece.color}></div>
+    {/each}
   </div>
 </div>
 {/if}
