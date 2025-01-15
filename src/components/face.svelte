12a25c5c4f687c786e059371d63b0705105b25bc
boladss <anton.boladss@gmail.com>
2025-01-15 16:32:51 +0800

    feat: include empty piece logic

--- src/components/face.svelte
+++ src/components/face.svelte
@@ -1,6 +1,6 @@
 <script>
   import { PLAYER_COLORS } from "$lib/types";
-  import { EmptyPiece, MovementPiece, SpecialPiece } from "$lib/types";
+  import { PieceType, EmptyPiece, MovementPiece, SpecialPiece } from "$lib/types";
   
   let { pieces, large = false } = $props();
   
@@ -12,7 +12,7 @@
 <div class="size-fit p-4 rounded-2xl bg-gray-900">
   <div class="grid grid-cols-2 grid-rows-2 shadow-[0_0_10px_0px]">
     {#each pieces as piece}
-      {#if piece.type === "MovementPiece"}
+      {#if piece.type === PieceType.Empty}
         <div 
           class="size-16 shadow-md {piece.getColor()} {interactable ? 'hover:shadow-[0_0_5px_5px_red] hover:z-10' : ''}" 
         >
